08f999df8a2f498a54d14823737d996d63bcb765
Heather Dice <heatherdice22@gmail.com>
2023-07-23 14:39:54 -0400

    Working on bg-vid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,13 @@ import backgroundVideo from "../assets/images/background.mp4";
 
 export default function Home() {
     return (
-        <div>
-            <video autoPlay loop muted className="fixed -z-1 w-full">
-                <source src={backgroundVideo} type="video/mp4" />
-            </video>
+        <div className="m-0">
+            <div className="relative z-1">
+                <h1>Heather Dice</h1>
+                <h2>Web Developer</h2>
+            </div>
+
+            <video playsInline autoPlay muted poster="background.png" className="fixed -z-1 w-screen h-screen object-cover top-0 left-0" src={backgroundVideo} type="video/mp4" />
         </div>
     );
 }
