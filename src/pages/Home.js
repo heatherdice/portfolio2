d2b9316d57ff225a91ce287c0522eb51abd22dd9
Heather Dice <heatherdice22@gmail.com>
2023-09-03 16:52:13 -0400

    Text gradient

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,8 @@ export default function Home() {
                 {/* position: absolute seems required for text overlay, but then text-end doesn't work
                     need to find a way to position text w/in overlay constraints */}
                     <div className="text-end">
-                        <h1 className="font-katibeh text-4xl md:text-9xl">Heather Dice</h1>
-                        <h2 className="font-karla -mt-10 text-5xl text-yale-blue">Web Developer</h2>
+                        <h1 className="font-katibeh text-4xl md:text-9xl bg-gradient-to-b from-oxford-blue from-2% to-french-gray to-50% text-transparent bg-clip-text">Heather Dice</h1>
+                        <h2 className="font-karla -mt-10 text-5xl text-oxford-blue">Web Developer</h2>
                     </div>
             </div>
             {/* <VideoBackground /> */}
