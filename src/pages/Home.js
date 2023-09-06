7bc559861b62af4634c85f6bda9bd1c181b4ccbc
Heather Dice <heatherdice22@gmail.com>
2023-09-06 17:03:55 -0400

    Simplified txt appearance w/ FadeIn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import VideoBackground from '../components/VideoBackground';
 import FadeIn from 'react-fade-in';
 
@@ -11,28 +11,16 @@ export default function Home() {
         subtitle: 'Web Developer'
     };
 
-    // set default state of title & subtitle to false; does not show until following useEffect function
-    const [showTitle, setShowTitle] = useState(false);
-
-    // text appears 5 seconds after load
-    useEffect(() => {
-        setTimeout(() => {
-            setShowTitle(true);
-        }, 5000);
-    }, []);
-
     return (
         <div className="main">
             <VideoBackground />
             <div className="content">
-                {showTitle &&
-                    <>
-                        <FadeIn>
-                            <h1 className="font-katibeh text-4xl md:text-9xl bg-gradient-to-b from-oxford-blue from-2% to-french-gray to-50% text-transparent bg-clip-text mr-10">{homeText.title}</h1>
-                            <h2 className="font-karla -mt-10 text-5xl text-oxford-blue mr-10">{homeText.subtitle}</h2>
-                        </FadeIn>
-                    </>
-                }
+                <FadeIn delay='2000' transitionDuration='3000'>
+                    <h1 className="font-katibeh text-4xl md:text-9xl bg-gradient-to-b from-oxford-blue from-2% to-french-gray to-50% text-transparent bg-clip-text mr-10">{homeText.title}</h1>
+                </FadeIn>
+                <FadeIn delay='3500' transitionDuration='3000'>
+                    <h2 className="font-karla -mt-10 text-5xl text-oxford-blue mr-10">{homeText.subtitle}</h2>
+                </FadeIn>
             </div>
         </div>
     );
