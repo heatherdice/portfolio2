fb1bab6524c263fce523b3b5297ffb8eea84d72e
Heather Dice <heatherdice22@gmail.com>
2023-09-06 17:07:11 -0400

    Removed props to simplify code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,21 +5,15 @@ import FadeIn from 'react-fade-in';
 // @TODO: organize CSS
 
 export default function Home() {
-    // homepage title, subtitle
-    const homeText = {
-        title: 'Heather Dice',
-        subtitle: 'Web Developer'
-    };
-
     return (
         <div className="main">
             <VideoBackground />
             <div className="content">
                 <FadeIn delay='2000' transitionDuration='3000'>
-                    <h1 className="font-katibeh text-4xl md:text-9xl bg-gradient-to-b from-oxford-blue from-2% to-french-gray to-50% text-transparent bg-clip-text mr-10">{homeText.title}</h1>
+                    <h1 className="font-katibeh text-4xl md:text-9xl bg-gradient-to-b from-oxford-blue from-2% to-french-gray to-50% text-transparent bg-clip-text mr-10">Heather Dice</h1>
                 </FadeIn>
                 <FadeIn delay='3500' transitionDuration='3000'>
-                    <h2 className="font-karla -mt-10 text-5xl text-oxford-blue mr-10">{homeText.subtitle}</h2>
+                    <h2 className="font-karla -mt-10 text-5xl text-oxford-blue mr-10">Web Developer</h2>
                 </FadeIn>
             </div>
         </div>
