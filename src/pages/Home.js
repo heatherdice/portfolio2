74c608cf6e25d3fd73e11732ba9c513dc4cee076
Heather Dice <heatherdice22@gmail.com>
2023-09-06 15:26:37 -0400

    Text loads after site page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import VideoBackground from '../components/VideoBackground';
 
 // @TODO: organize CSS
 
 export default function Home() {
+    // homepage title, subtitle
+    const homeText = {
+        title: 'Heather Dice',
+        subtitle: 'Web Developer'
+    };
+
+    // set default state of title & subtitle to false; does not show until following useEffect function
+    const [showTitle, setShowTitle] = useState(false);
+
+    // text appears 5 seconds after load
+    useEffect(() => {
+        setTimeout(() => {
+            setShowTitle(true);
+        }, 5000);
+    }, []);
+
     return (
         <div className="main">
             <VideoBackground />
             <div className="content">
-                <h1 className="font-katibeh text-4xl md:text-9xl bg-gradient-to-b from-oxford-blue from-2% to-french-gray to-50% text-transparent bg-clip-text mr-10">Heather Dice</h1>
-                <h2 className="font-karla -mt-10 text-5xl text-oxford-blue mr-10">Web Developer</h2>
+                {showTitle &&
+                    <>
+                        <h1 className="font-katibeh text-4xl md:text-9xl bg-gradient-to-b from-oxford-blue from-2% to-french-gray to-50% text-transparent bg-clip-text mr-10">{homeText.title}</h1>
+                        <h2 className="font-karla -mt-10 text-5xl text-oxford-blue mr-10">{homeText.subtitle}</h2>
+                    </>
+                }
             </div>
         </div>
     );
