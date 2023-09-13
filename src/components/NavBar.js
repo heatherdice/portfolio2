507b580252abb7254d30b137fd20bff21d74b95f
Heather Dice <heatherdice22@gmail.com>
2023-09-13 07:05:46 -0400

    Cleaned up nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,27 @@ import "../css/NavBar.css";
 import useResponsive from "../hooks/useResponsive";
 
 export default function NavBar(width, height) {
+    // navbar links and titles as objects, put in array to map over for cleaner return code
+    const about = {
+        link: '/about',
+        title: 'About'
+    };
+    const projects = {
+        link: '/projects',
+        title: 'Projects'
+    };
+    const experience = {
+        link: '/experience',
+        title: 'Experience'
+    };
+    const contact = {
+        link: '/contact',
+        title: 'Contact'
+    };
+    const navLinksArray = [
+        about, projects, experience, contact
+    ];
+
     // responsive settings for mobile - good, but do I need to add this to every component that changes depending on mobile screen? What if something just needs to be resized?
     const {screenType} = useResponsive();
     const [mobileView, setMobileView] = useState(false);
@@ -31,18 +52,13 @@ export default function NavBar(width, height) {
                     {!mobileView ? (
                         <ul className="flex flex-row justify-end font-karla font-medium text-3xl text-oxford-blue">
                             {/* classes on li's not working, except padding? */}
-                            <li className="nav-link cursor-pointer p-5">
-                                <Link to='/about'>About</Link>
-                            </li>
-                            <li className="nav-link cursor-pointer p-5">
-                                <Link to='/projects'>Projects</Link>
-                            </li>
-                            <li className="nav-link cursor-pointer p-5">
-                                <Link to='/experience'>Experience</Link>
-                            </li>
-                            <li className="nav-link cursor-pointer p-5">
-                                <Link to='/contact'>Contact</Link>
-                            </li>
+                            {navLinksArray.map((navbar) =>
+                                <li className="nav-link cursor-pointer p-5">
+                                    <Link to={navbar.link} >
+                                        {navbar.title}
+                                    </Link>
+                                </li>
+                            )}
                         </ul>
                     ) : (
                         <ul className="flex justify-end">
