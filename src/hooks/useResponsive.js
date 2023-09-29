13447bcfaa5df01f41f52ff6cb1f18fe7c0e7e0f
Heather Dice <heatherdice22@gmail.com>
2023-09-29 15:14:18 -0400

    Update/clean up useResponsive Hook

--- src/hooks/useResponsive.js
+++ src/hooks/useResponsive.js
@@ -4,22 +4,25 @@ import { useState, useEffect } from "react";
 export default function useResponsive() {
     // set default state of screen resolution to false
     const [screenType, setScreenType] = useState({
-        mobileScreen: window.innerWidth <= 480,
-        tabletScreen: window.innerWidth >= 480 && window.innerWidth <= 750,
-        desktopScreen: window.innerWidth > 750
+        mobileScreen: false,
+        tabletScreen: false,
+        desktopScreen: false
     });
 
     // update state whenever user resizes the screen
     const updateWindowWidth = () => {
-        const mobileScreen = window.innerWidth <= 480;
-        const tabletScreen = window.innerWidth >= 480 && window.innerWidth <= 750;
-        const desktopScreen = window.innerWidth > 750;
+        const mobileWidth = window.innerWidth <= 480;
+        const tabletWidth = window.innerWidth >= 480 && window.innerWidth <= 750;
+        const desktopWidth = window.innerWidth > 750;
 
-        setScreenType({mobileScreen, tabletScreen, desktopScreen});
+        setScreenType({mobileScreen: mobileWidth, tabletScreen: tabletWidth, desktopScreen: desktopWidth});
     };
 
     // update state on initial load
     useEffect(() => {
+        // initial screen size check to apply initial styling
+        updateWindowWidth();
+
         // listen for window resize, call updateWindowWidth
         window.addEventListener("resize", updateWindowWidth);
 
@@ -27,7 +30,7 @@ export default function useResponsive() {
         return function cleanup() {
             window.removeEventListener("resize", updateWindowWidth);
         };
-    }, [window.innerWidth]); // tells React to listen to any changes in window.innerWidth in eventListener in order to call useEffect Hook, rather than running after every render regardless of whether or not window was resized
+    }, []);
 
     return screenType
 };
