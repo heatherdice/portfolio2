bd7f158a79b18526c5e4d2089bf78bcc0d2a269e
Heather Dice <heatherdice22@gmail.com>
2023-11-14 20:07:29 -0500

    Added About page, route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar.js';
 import Home from './pages/Home.js';
 import VideoBackground from './components/VideoBackground';
+import About from '.pages/About.js';
 
 export default function App() {
 
@@ -12,6 +13,7 @@ export default function App() {
       <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </div>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div className="grid grid-cols-2">
+
+        </div>
+    );
+};
