8c67461f47c76560d31b7c8b71e7a052dc217c98
Heather Dice <heatherdice22@gmail.com>
2023-11-20 21:39:38 -0500

    Background img style, more legible text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,19 @@
 import React from "react";
-import background from "../assets/images/background.png";
+import background from "../assets/images/paintedBackground.png";
 import profilePic from "../assets/images/profilepic.jpg";
 
 export default function About() {
     return (
-        <div style={{ backgroundImage: `url(${background})`, height: "100vh", width: "100vh" }}>
-            <div className="absolute m-[10%] flex flex-col justify-center items-center font-karla">
+        <div style={{ 
+            backgroundImage: `url(${background})`, 
+            height: "100vh", 
+            backgroundPosition: "center" 
+        }}>
+            <div className="absolute m-[8%] flex flex-col justify-center items-center font-karla">
                 <div className="grid grid-cols-2 gap-5 text-oxford-blue">
                     <div className="text-left">
-                        <h1 className="text-7xl leading-loose">Hey, I'm Heather!</h1>
-                        <p className="text-2xl">I'm a fullstack developer with a particular interest in frontend work.</p>
+                        <h1 className="text-7xl leading-loose font-bold">Hey, I'm Heather!</h1>
+                        <p className="text-3xl font-semibold">I'm a fullstack developer with a particular interest in frontend work.</p>
                     </div>
                     <div className="h-[75%] w-[75%] flex justify-center content-center">
                         <img className="rounded-md shadow-lg" src={profilePic} alt="profile picture" />
