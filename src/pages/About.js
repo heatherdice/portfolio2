7cf2badb6cba3ae78878fb66667ba33b06b4e231
Heather Dice <heatherdice22@gmail.com>
2023-11-20 21:23:20 -0500

    Changed font, added some text

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,10 +3,11 @@ import profilePic from "../assets/images/profilepic.jpg";
 
 export default function About() {
     return (
-        <div className="absolute m-[10%] flex flex-col justify-center items-center">
-            <div className="grid grid-cols-2 gap-5">
-                <div className="text-2xl">
-                    This will be where the summary goes.
+        <div className="absolute m-[10%] flex flex-col justify-center items-center font-karla">
+            <div className="grid grid-cols-2 gap-5 text-oxford-blue">
+                <div className="text-left">
+                    <h1 className="text-7xl leading-loose">Hey, I'm Heather!</h1>
+                    <p className="text-2xl">I'm a fullstack developer with a particular interest in frontend work. </p>
                 </div>
                 <div className="h-[75%] w-[75%] flex justify-center content-center">
                     <img className="rounded-md shadow-lg" src={profilePic} alt="profile picture" />
