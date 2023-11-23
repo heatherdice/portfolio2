df4fd11f04eadaa4ba2d8ac5afa6ead84377a20e
Heather Dice <heatherdice22@gmail.com>
2023-11-23 10:07:24 -0500

    Improved alignment, sizing in desktop view

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,13 +15,13 @@ export default function About() {
             height: "100vh", 
             backgroundPosition: "center" 
         }}>
-            <div className="absolute m-[8%] flex flex-col justify-center items-center font-karla">
-                <div className="grid grid-cols-2 gap-5 text-oxford-blue">
-                    <div className="text-left">
+            <div className="absolute mt-[8%] mx-[3%] flex flex-col justify-center font-karla">
+                <div className="grid grid-cols-3 gap-5 text-oxford-blue">
+                    <div className="text-left col-span-2">
                         <h1 className="text-7xl leading-loose font-bold">Hey, I'm Heather!</h1>
                         <p className="text-3xl font-semibold">I'm a music teacher-turned fullstack developer from the Philadelphia area with a particular interest in frontend work. I am currently working for Altus Technology Solutions as a junior web developer.</p>
                     </div>
-                    <div className="h-[75%] w-[75%] flex justify-center content-center">
+                    <div className="flex justify-center col-span-1 p-[15%]">
                         <img className="rounded-md shadow-lg" src={profilePic} alt="profile picture" />
                     </div>
                 </div>
